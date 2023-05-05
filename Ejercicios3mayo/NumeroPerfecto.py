6af6f70d45c92f792943fc2aacbcc73ddcbef86c
dubancorrea <dubanfelipecorreagarzon@gmail.com>
2023-05-04 21:29:16 -0500

    Ejercicios 3 de mayo terminados

--- Ejercicios3mayo/NumeroPerfecto.py
+++ Ejercicios3mayo/NumeroPerfecto.py
@@ -1,10 +1,11 @@
-nump=int(input("Ingrese un numero: "))
-for i in range(2, nump+1):
-    a=0
-    for j in range(1, (i//2)+1):
-        if((i%j)==0):
-            a= a+j
-    if(a==i):
-        print("%s es perfecto" %i)
-    else:
-        print("%s no es perfecto" %i)
+n=int(input("Ingrese un numero: "))
+num1=6
+num2=8
+sum=0
+for i in range (1, n):
+    if n%i == 0:
+        sum+=i
+if sum==n: 
+    print(f"{n} es un numero perfecto")
+else:
+    print(f"{n} no es numero perfecto")
